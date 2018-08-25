316a5a6ae9d7fcce487cf4f1d5e208051a3fcdad
Yusuke Ohashi <steveohashi@gmail.com>
2018-08-26 05:23:46 +0900

    Added first UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import {app, BrowserWindow} from 'electron'
+import * as path from 'path'
 
-let win
+let win: BrowserWindow
 
-app.on('ready', () => {
-  win = new BrowserWindow({
-    darkTheme: true
-  })
+function createWindow() {
+    win = new BrowserWindow({
+      darkTheme: true
+    })
 
-  app.setBadgeCount(1)
-})
+    win.loadFile(path.join(__dirname, '../index.html'))
+
+    win.webContents.openDevTools()
+
+    win.on('close', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
